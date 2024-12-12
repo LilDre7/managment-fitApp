8a0baaafbeefacdddd92a815678d17c3b1246618
Alvaro Aburto Ocampo <109699805+LilDre7@users.noreply.github.com>
2024-12-12 22:42:56 +0000

    :fire: Remove code or files

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+// Creo que esto cambiar bro
+
 import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
